95f0032b3299c81092c637c57ad03d06ad444eb8
Alif <79266506+S-Alif@users.noreply.github.com>
2023-09-03 20:48:06 +0600

    update: improved queries

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose')
-
 const productModel = require('../models/prodcutModel')
 const cartModel = require('../models/cartItemModel')
+const orderModel = require('../models/orderModel')
 
 // create a product
 exports.createProduct = async (req, res) => {
@@ -39,7 +38,7 @@ exports.enterItemToCart = async (req, res) => {
   }
 }
 
-// carts
+// retrieve carts
 exports.retrieveCart = async (req, res) => {
   try {
     let id = req.params.id
@@ -47,14 +46,14 @@ exports.retrieveCart = async (req, res) => {
       { $match: { $expr: { $eq: ['$user', { $toObjectId: id }] } } },
       {
         $lookup: {
-          from: "users", localField: "user", foreignField: { "_id": id}, pipeline: [{
+          from: "users", localField: "user", foreignField:"_id", pipeline: [{
             $project: {
               firstName: 1,
               lastName: 1,
               _id: 0
             }
           }],
-          as: "user"
+          as: "userData"
         }
       },
       {
@@ -63,27 +62,120 @@ exports.retrieveCart = async (req, res) => {
             $project: {
               name: 1,
               imageURL: 1,
+              price: 1,
               _id: 0
             }
           }],
-          as: "product"
+          as: "productData"
         }
       },
-
-      { $unwind: "$user" },
-      { $unwind: "$product" },
-
+      
       {
-        $group: {
-          // user: {$push: "$user"},
-          product: {$push: "$product"}
+        $project:{
+          _id: 0,
+          createdAt: 0,
+          updatedAt: 0,
         }
-      }
+      },
+      { $unwind: "$userData" },
+      { $unwind: "$productData" },
     ])
 
     res.status(200).json({
       status: 1,
       data: cart
+    })
+
+  } catch (error) {
+    res.status(200).json({
+      status: 0,
+      data: error
+    })
+  }
+}
+
+// order
+exports.postOrder = async (req, res) => {
+  try {
+    let order = await orderModel.create(req.body)
+
+    res.status(200).json({
+      status: 1,
+      data: order
+    })
+
+  } catch (error) {
+    res.status(200).json({
+      status: 0,
+      data: error
+    })
+  }
+}
+
+// get order
+exports.getOrder = async (req, res) => {
+  try {
+    let id = req.params.id
+    let order = await orderModel.aggregate([
+      { $match: { $expr: { $eq: ['$user', { $toObjectId: id }] } } },
+      {
+        $lookup: {
+          from: "users", localField: "user", foreignField: "_id", pipeline: [{
+            $project: {
+              firstName: 1,
+              lastName: 1,
+              _id: 0
+            }
+          }],
+          as: "userData"
+        }
+      },
+      {
+        $lookup: {
+          from: "cartitems", localField: "user", foreignField: "user", pipeline: [
+            {
+              $lookup: {
+                from: 'products',
+                localField: 'product',
+                foreignField: '_id',pipeline: [
+                  {
+                    $project: {
+                      name: 1,
+                      price: 1,
+                      imageURL: 1,
+                      _id: 0
+                    }
+                  }
+                ],
+                as: 'productDetails'
+              }
+            },
+            {
+            $project: {
+              quantity:1,
+              productDetails: 1,
+              _id: 0
+            }
+          }
+        ],
+          as: "productData"
+        }
+      },
+      {
+        $project:{
+          _id: 0,
+          user: 0,
+          items: 0,
+          createdAt: 0,
+          updatedAt: 0
+        }
+      },
+      {$unwind: "$userData"}
+    ])
+
+    res.status(200).json({
+      status: 1,
+      data: order
     })
 
   } catch (error) {
